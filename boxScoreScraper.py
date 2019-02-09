88255ce06a94233ebfd00c60ca5ccc5bee121888
abhig1997 <gupta257@purdue.edu>
2019-02-09 17:08:00 -0500

    ran successful test on Tim Duncan stats

--- boxScoreScraper.py
+++ boxScoreScraper.py
@@ -155,3 +155,4 @@ if __name__ == "__main__":
     # getAllNbaPlayers()
     stats = getPlayerRegularSeasonStats(1495)
     # getPlayerNameFromId(1495)
+    writeRegularSeasonStatsToCsv("Tim Duncan", stats)
