9860e397d1bba30f247da8f9ad6af27eaaef910a
Sidhant Chadda <sidhantchadda@gmail.com>
2019-02-11 23:27:21 -0500

    adding documentation

--- initCron.sh
+++ initCron.sh
@@ -1,11 +1,17 @@
 #remove existing crontab file if present
 crontab -r 2> /dev/null
 
-
+#stores filepath such that this can be run in any bash enviornment
 FILEPATH="$(pwd)"
 
 echo "MAILTO=\"sidhantchadda@gmail.com\"" > cron
+
+#runs every day at midnight
 echo "00 00 * * * python /Users/sidhantchadda/NPS/"$pwd"boxScoreScraper.py" >> cron
 
+
+#start cronjob
 crontab cron
+
+#remove temporary file
 rm cron
