42c32a5a96aa470fb0c26308e448d48685b7c11b
abhig1997 <gupta257@purdue.edu>
2019-03-01 18:14:56 -0500

    added file, added method to read in game logs for a team

---
+++ gameOutcomePredictor.py
@@ -0,0 +1,31 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import pickle
+import sys
+
+def load_dataset(filename):
+    """
+    Given a filename pointing to a CSV file that contains game logs for a
+    certain team, returns a pandas dataframe containing the values
+    :param filename: CSV file to read in
+    :return: Pandas dataframe
+    """
+    dataset = pandas.read_csv(filename, header=[0])
+    return dataset
+
+
+
+if __name__ == "__main__":
+    dataset = load_dataset("datasets/ATL_2015_to_2018.csv")
+    print(dataset.head(5))
